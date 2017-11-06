bc7a100590812920148f398c0dacb4bca352bdf8
Mike0_000 <Mike0_000@MikePC>
2017-11-06 13:50:33 -0500

    testing

---
+++ IST311GroupProject/src/ist311groupproject/IST311GroupProject.java
@@ -0,0 +1,13 @@
+
+package ist311groupproject;
+
+public class IST311GroupProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Mike... testing
+    }
+    
+}
