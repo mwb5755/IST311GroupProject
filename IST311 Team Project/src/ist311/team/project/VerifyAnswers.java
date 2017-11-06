c3c54bb5341becc15e7567a019e8acdc6a191a27
Adrian <avg5074@gmail.com>
2017-11-06 13:00:02 -0500

    Created VerifyAnswers class to check user input

---
+++ IST311 Team Project/src/ist311/team/project/VerifyAnswers.java
@@ -0,0 +1,13 @@
+
+package ist311.team.project;
+
+/**
+ *
+ * @author Adrian
+ * 
+ * This class will take user input and verify whether it is the correct answer
+ * or not.
+ */
+public class VerifyAnswers {
+    
+}
