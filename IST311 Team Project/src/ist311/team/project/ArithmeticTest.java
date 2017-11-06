c51d41e380173978698822482212924fd750fc79
Adrian <avg5074@gmail.com>
2017-11-06 13:19:25 -0500

    ArithimeticTest - *instantiated a new user to display a welcome

--- IST311 Team Project/src/ist311/team/project/ArithmeticTest.java
+++ IST311 Team Project/src/ist311/team/project/ArithmeticTest.java
@@ -7,8 +7,19 @@ package ist311.team.project;
  * 
  * This will be the main tester for our application
  */
+import java.util.Scanner;
+
 public class ArithmeticTest {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        GreetingDisplay user = new GreetingDisplay();
+        System.out.println("What is your name?");
+        String name = input.next();
+        user.setName(name);
+        user.display(name);
+        
+        
         
     }
     
