aca0c1ef265747b046ff5b973254d3cb1643d9e4
Adrian <avg5074@gmail.com>
2017-11-06 13:00:22 -0500

    ArithmeticTest will be our main class that runs the application

---
+++ IST311 Team Project/src/ist311/team/project/ArithmeticTest.java
@@ -0,0 +1,15 @@
+
+package ist311.team.project;
+
+/**
+ *
+ * @author Adrian
+ * 
+ * This will be the main tester for our application
+ */
+public class ArithmeticTest {
+    public static void main(String[] args) {
+        
+    }
+    
+}
