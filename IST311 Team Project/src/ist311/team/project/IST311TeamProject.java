6238b1bbb8e307cbce2bcb65262e790f1ed2eac3
Adrian <avg5074@gmail.com>
2017-11-06 12:53:08 -0500

    Test

---
+++ IST311 Team Project/src/ist311/team/project/IST311TeamProject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ist311.team.project;
+
+/**
+ *
+ * @author Adrian
+ */
+public class IST311TeamProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // starting
+    }
+    
+}
