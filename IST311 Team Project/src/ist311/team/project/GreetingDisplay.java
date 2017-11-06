5c89472a45a65c3e774ba3d78a5ce8d455661aea
Adrian <avg5074@gmail.com>
2017-11-06 13:18:45 -0500

    GreetingDisplay class will display a greeting message to the user *added some methods to get/set name and disapay a welcome

--- IST311 Team Project/src/ist311/team/project/GreetingDisplay.java
+++ IST311 Team Project/src/ist311/team/project/GreetingDisplay.java
@@ -1,11 +1,28 @@
-
 package ist311.team.project;
 
 /**
  *
- * @author Adrian
- * This class will display a greeting message to the user
+ * @author Adrian This class will display a greeting message to the user
  */
 public class GreetingDisplay {
-    
+
+    private String userName;
+
+    public GreetingDisplay() {
+        this.userName = userName;
+    }
+
+    public void setName(String name) {
+        this.userName = name;
+    }
+
+    public String getName() {
+        return userName;
+    }
+
+    public void display(String name) {
+        System.out.println("==Arithmetic Test==");
+        System.out.println("Welcome " + name + " are you ready to begin?");
+    }
+
 }
